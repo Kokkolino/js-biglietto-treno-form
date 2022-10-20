f1c8bf2d0bef40a94828bda91686cef3f4da2980
Francesco C <fran461j@outlook.it>
2022-10-20 15:48:25 +0200

    ticket card

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,10 +3,10 @@ function check (){
     let age = parseInt(document.getElementById('age').value);
     let distance = parseFloat(document.getElementById('distance').value);
     let price = distance * priceonkm.toFixed(2);
-    if(age<underaged){
+    if(age===1){
         price = (price -((price / 100)*20)).toFixed(2);
     }
-    else if(age>=elderly)
+    else if(age===3)
     {
         price = (price -((price / 100)*40)).toFixed(2);
     }
