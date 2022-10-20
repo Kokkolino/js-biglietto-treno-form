40482686c923408c56cac557ffee4414a2f63176
Francesco C <fran461j@outlook.it>
2022-10-20 14:41:33 +0200

    recycled previus exercise :D

---
+++ assets/js/main.js
@@ -0,0 +1,16 @@
+const underaged = 18;
+const elderly = 65;
+const priceonkm = 0.21;
+function check (){
+    let age = parseInt(document.getElementById('age').value);
+    let distance = parseFloat(document.getElementById('distance').value);
+    let price = distance * priceonkm.toFixed(2);
+    if(age<underaged){
+        price = (price -((price / 100)*20)).toFixed(2);
+    }
+    if(age>=elderly)
+    {
+        price = (price -((price / 100)*40)).toFixed(2);
+    }
+    document.getElementById('finalprice').innerHTML =`Your price is ${price}. Enjoy your trip!`;
+}
